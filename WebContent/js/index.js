e0a3446d8ea37bcc7620800a40b74ac86725d67e
Man Parvesh Singh Randhawa <randhawa_m@worksap.co.jp>
2016-11-04 17:17:55 +0530

    minor UI tweaks and add real-time search for name

--- WebContent/js/index.js
+++ WebContent/js/index.js
@@ -1,29 +1,43 @@
-// parse request params
-var q1 = $.url().param('q1');
-$('input[name="q1"]').val(q1);
-var q2 = $.url().param('q2');
-$('input[name="q2"]').val(q2);
-
 // read data from database
+var q;
 var emps;
-if (q1) {
-	emps = alasql('SELECT * FROM emp WHERE number LIKE ?', [ '%' + q1 + '%' ]);
-} else if (q2) {
-	emps = alasql('SELECT * FROM emp WHERE name LIKE ?', [ '%' + q2 + '%' ]);
+if (q) {
+	emps = alasql('SELECT * FROM emp WHERE number LIKE ?', [ '%' + q + '%' ]);
 } else {
 	emps = alasql('SELECT * FROM emp', []);
+    // create employee list
+    var tbody = $('#tbody-emps');
+    tbody.empty();
+    for (var i = 0; i < emps.length; i++) {
+        var emp = emps[i];
+        var tr = $('<tr></tr>');
+        tr.append('<td><img height=40 class="img-circle" src="img/' + emp.id + '.jpg"></td>');
+        tr.append('<td><a href="emp.html?id=' + emp.id + '">' + emp.number + '</a></td>');
+        tr.append('<td>' + emp.name + '</td>');
+        tr.append('<td>' + DB.choice(emp.sex) + '</td>');
+        tr.append('<td>' + emp.birthday + '</td>');
+        tr.append('<td>' + emp.tel + '</td>');
+        tr.appendTo(tbody);
+    }
 }
 
-// create employee list
-var tbody = $('#tbody-emps');
-for (var i = 0; i < emps.length; i++) {
-	var emp = emps[i];
-	var tr = $('<tr></tr>');
-	tr.append('<td><img height=40 class="img-circle" src="img/' + emp.id + '.jpg"></td>');
-	tr.append('<td><a href="emp.html?id=' + emp.id + '">' + emp.number + '</a></td>');
-	tr.append('<td>' + emp.name + '</td>');
-	tr.append('<td>' + DB.choice(emp.sex) + '</td>');
-	tr.append('<td>' + emp.birthday + '</td>');
-	tr.append('<td>' + emp.tel + '</td>');
-	tr.appendTo(tbody);
-}
+$('#input-search').on('input',function(){
+    q = $(this).val();
+    emps = alasql('SELECT * FROM emp WHERE name LIKE ?', [ '%' + q + '%' ]);
+    // create employee list
+    var tbody = $('#tbody-emps');
+    tbody.empty();
+    for (var i = 0; i < emps.length; i++) {
+        var emp = emps[i];
+        var tr = $('<tr></tr>');
+        tr.append('<td><img height=40 class="img-circle" src="img/' + emp.id + '.jpg"></td>');
+        tr.append('<td><a href="emp.html?id=' + emp.id + '">' + emp.number + '</a></td>');
+        tr.append('<td>' + emp.name + '</td>');
+        tr.append('<td>' + DB.choice(emp.sex) + '</td>');
+        tr.append('<td>' + emp.birthday + '</td>');
+        tr.append('<td>' + emp.tel + '</td>');
+        tr.appendTo(tbody);
+    }
+});
+
+

c90ecb4d2c3778eb17ac826d0ab5848c61734804
Man Parvesh Singh Randhawa <randhawa_m@worksap.co.jp>
2016-11-08 14:54:05 +0530

    Add to compare - (30%)

--- WebContent/js/index.js
+++ WebContent/js/index.js
@@ -1,6 +1,12 @@
 // read data from database
 var q;
 var emps;
+
+var compareIDs = [];
+var compareNumber = 0;
+
+var colors = ['#ffb3ff', '#b3ecff', '#b3ffb3', '#fad581', '#ff9a9a'];
+
 if (q) {
 	emps = alasql('SELECT * FROM emp WHERE number LIKE ?', [ '%' + q + '%' ]);
 } else {
@@ -10,7 +16,8 @@ if (q) {
     tbody.empty();
     for (var i = 0; i < emps.length; i++) {
         var emp = emps[i];
-        var tr = $('<tr></tr>');
+        var tr = $('<tr id="row-' + emp.id + '"></tr>');
+        tr.append('<td><input type="checkbox" name="checkbox-' + emp.id + '" id="checkbox-' + emp.id + '" onclick="comparisonProcedure(' + emp.id + ')"></td>');
         tr.append('<td><img height=40 class="img-circle" src="img/ (' + emp.id + ').jpg"></td>');
         tr.append('<td><a href="emp.html?id=' + emp.id + '">' + emp.number + '</a></td>');
         tr.append('<td>' + emp.name + '</td>');
@@ -39,7 +46,8 @@ $('#input-search').on('input',function(){
     tbody.empty();
     for (var i = 0; i < emps.length; i++) {
         var emp = emps[i];
-        var tr = $('<tr></tr>');
+        var tr = $('<tr id="row-' + emp.id + '"></tr>');
+        tr.append('<td><input type="checkbox" name="checkbox-' + emp.id + '" id="checkbox-' + emp.id + '" onclick="comparisonProcedure(' + emp.id + ')"></td>');
         tr.append('<td><img height=40 class="img-circle" src="img/ (' + emp.id + ').jpg"></td>');
         tr.append('<td><a href="emp.html?id=' + emp.id + '">' + emp.number + '</a></td>');
         tr.append('<td>' + emp.name + '</td>');
@@ -48,6 +56,7 @@ $('#input-search').on('input',function(){
         tr.append('<td>' + emp.tel + '</td>');
         tr.appendTo(tbody);
     }
+    updateCheckBoxes();
 });
 
 //$('#search-col').text()
@@ -62,3 +71,100 @@ $('#cols-name').on('click',function(){
 $('#cols-skills').on('click',function(){
     $('#search-col').text('Skills');
 });
+
+
+function comparisonProcedure(id){
+    if(compareIDs.includes(id)){
+        removeFromCompare(id);
+    }else{
+        addToCompare(id);
+    }
+}
+
+function addToCompare(id){
+    compareIDs.push(id);
+    compareNumber++;
+    if(checkIfCompareIsFull()){
+        disableOtherCheckboxes();
+    }
+    //showIDs();
+    ui();
+}
+
+function removeFromCompare(id){
+    if(checkIfCompareIsFull()){
+        enableAllCheckBoxes();
+    }
+    compareIDs.splice(compareIDs.indexOf(id),1);
+    compareNumber--;
+    $('#row-'+id).css('background-color', 'white');
+    ui();
+}
+
+function showIDs(){
+    alert(compareIDs);
+}
+
+function checkIfCompareIsFull(){
+    return compareNumber >= 5;
+}
+
+function ui(){
+    showComparisonMenuAtTheTop();
+    setBackgroundColorOfSelectedIDsForComparison();
+}
+
+function showComparisonMenuAtTheTop(){
+    //TODO----------------------------------------------------------------------------------------------
+}
+
+function setBackgroundColorOfSelectedIDsForComparison(){
+    for(var i=0;i<compareIDs.length;i++){
+        var id = compareIDs[i];
+        $('#row-'+id).css('background-color', colors[i]);
+    }
+}
+
+function disableOtherCheckboxes(){
+    for(var i=0;i<emps.length;i++){
+        if(!compareIDs.includes(emps[i].id)){
+            disableCheckbox(emps[i].id);
+        }
+    }
+}
+
+function enableAllCheckBoxes(){
+    for(var i=0;i<emps.length;i++){
+        enableCheckbox(emps[i].id);
+    }
+}
+
+function disableCheckbox(id){
+    document.getElementById('checkbox-'+id).disabled = true;
+}
+
+function enableCheckbox(id){
+    document.getElementById('checkbox-'+id).disabled = false;
+}
+
+function updateCheckBoxes(){
+    for(var i = 0;i < compareIDs.length;i++){
+        var id = compareIDs[i];
+        if(isIDThere(id)){
+            document.getElementById('checkbox-'+id).checked = true;
+        }
+    }
+    if(checkIfCompareIsFull()){
+        disableOtherCheckboxes();
+    }
+}
+
+function isIDThere(id){
+    for(var i=0;i<emps.length;i++){
+        var emp = emps[i];
+        if(emp.id === id){
+            return true;
+        }
+    }
+    return false;
+}
